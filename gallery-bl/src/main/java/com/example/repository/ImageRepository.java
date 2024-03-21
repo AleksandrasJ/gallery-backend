0958c2f7cbd83338609240bc0ec8dd181c8013cc
Aleksandras Juzumas <aleksandras.juzumas@insoft.lt>
2024-03-21 10:12:06 +0200

    refactor: cleanup

--- gallery-bl/src/main/java/com/example/repository/ImageRepository.java
+++ gallery-bl/src/main/java/com/example/repository/ImageRepository.java
@@ -3,10 +3,7 @@ package com.example.repository;
 import com.example.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Long>,
